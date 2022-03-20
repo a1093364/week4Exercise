a8b9de761315b87c9d7596bd2a263e2126b5d719
a1093364 <100330639+a1093364@users.noreply.github.com>
2022-03-20 19:29:34 +0800

    Add files via upload

---
+++ sign_in_failed.php
@@ -0,0 +1,10 @@
+<html>
+<title>登入失敗</title>
+    <body  background='驚嘆號.jpg'>
+        </br>
+        </br>
+        <font color="red" size="7" face="標楷體"><center><b>登入失敗</b></font></br></br>
+        <a href="register.php" target="_self"> 註冊表單 </a></center>
+        
+    </body>
+</html>
